624b2344c3d0dc276e2158674ac57fe7e3f8eaec
Melyre <ariel.roman@hotmail.fr>
2014-03-28 17:03:57 +0100

    Update interpreter.cpp

---
+++ src/matrix++/interpreter.cpp
@@ -0,0 +1,29 @@
+#incldue <iostream>
+
+using namespce std;
+
+Interpreter::Interpreter()
+{
+  command="";
+}
+
+Interpreteur::~Interpreter()
+{
+  
+}
+
+void Interpreter::setCommand(string &command)
+{
+  this.command=command;
+}
+
+string Interpreter::getCommand()
+{
+  if (command == "") { return "Aucune commande en memoire" }
+  else { return command; }
+}
+
+void Interpreter::activeProcess(MatrixManager &matrixManager)
+{
+  
+}
