a3b79609b0da475a2f0f4f0a6b554a8a598727fe
Melyre <ariel.roman@hotmail.fr>
2014-03-28 18:12:54 +0100

    Update interpreter.h

--- headers/matrix++/interpreter.h
+++ headers/matrix++/interpreter.h
@@ -8,18 +8,19 @@ using namespace std;
 
 class Interpreter
 {
-private:
-	string command;
-public:
-	Interpreter();
-	~Interpreter();
-
-	void activeProcess(MatrixManager & matrixManager);	// Traite la commande
-	// Et active les fonctions adéquates au manager de matrice.
-	/* Exemple :
-	Si la commande est C = A * B
-	Il va faire matrixManager.multMatrix(matrixManager("A", "B", "C")); */
+	private:
+		string command;
+	public:
+		Interpreter();
+		~Interpreter();
+		void activeProcess(MatrixManager & matrixManager);	// Traite la commande
+		// Et active les fonctions adéquates au manager de matrice.
+		/* Exemple :
+		Si la commande est C = A * B
+		Il va faire matrixManager.multMatrix(matrixManager("A", "B", "C")); */
 	
-	void setCommand(string &command);	// Permet de donner une commandeà l'interpréteur.
-	string getCommand();			// Permet de récupérer la commande de l'interpréteur.
+		void setCommand(string &command);	// Permet de donner une commandeà l'interpréteur.
+		string getCommand();			// Permet de récupérer la commande de l'interpréteur.
 };
+
+#endif
