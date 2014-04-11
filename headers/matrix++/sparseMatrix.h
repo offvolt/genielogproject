259d953bae474e3519fcb0802bd683b912a5a8be
Herlderlord <idolon@voila.fr>
2014-04-11 12:32:15 +0200

    Commentaires doxygen ajoutés.

--- headers/matrix++/sparseMatrix.h
+++ headers/matrix++/sparseMatrix.h
@@ -1,3 +1,12 @@
+/// \file sparseMatrix.h
+/// \brief Header de la classe SparseMatrix
+/// Cette classe est notre entité de base sur laquelle nous
+/// allons travailler. Elle permet de faire un lien avec un 
+/// fichier sparseMatrix.
+/// \author Mathias.Q
+/// \date 11/04/2014
+
+
 #ifndef H_SPARSEMATRIX
 #define H_SPARSEMATRIX
 
@@ -16,127 +25,142 @@ private:
 
 public:
 
-	//! Constructeur vide
-	/*!
-		Un constructeur qui ne lit pas l'objet à un fichier. 
-		L'instance ne vaut donc rien, c'est un objet vide,
-		non paramétré.
-		\sa
-	*/
+	/// \fn SparseMatrix()
+	/// \brief Constructeur vide
+	///
+	/// Un constructeur qui ne lit pas l'objet à un fichier. 
+	/// L'instance ne vaut donc rien, c'est un objet vide,
+	/// non paramétré.
+	/// \sa SparseMatrix(string const& fileName)
+
 	SparseMatrix();	
 	
-	//! Constructeur 
-	/*!
-		\sa SparseMatrix()
-		\param fileName le nom du fichier.
-	*/
-	SparseMatrix(string &fileName);				///< Constructure faisant référence à un fichier, elle en récupère ses champs.
+	/// \fn SparseMatrix(string const& fileName)
+	/// \brief Constructeur 
+	///
+	/// \sa SparseMatrix()
+	/// \param fileName le nom du fichier.
+	///
+	SparseMatrix(string const& fileName);				///< Constructure faisant référence à un fichier, elle en récupère ses champs.
 	
-	//! Destructeur
-	/*!
-		Un destructeur qui n'a pas grande utilité ici.
-	*/
+	/// \fn ~SparseMatrix()
+	/// \brief Destructeur
+	///
+	/// Un destructeur qui n'a pas grande utilité ici.
+	///
 	~SparseMatrix();
 
-	//! Création d'une nouvelle matrice.
-	/*!
-		Va créer un nouveau fichier et initialiser les valeurs 
-		width et heigth. 
-		Attention : On ne crée rien si le fichier existe déjà.
-		\param width la largeur de la matrice.
-		\param height la hauteur de la matrice.
-		\param fileName le nom du fichier que l'on veut créer.
-	*/	
-	bool newMatrix(unsigned int &width, unsigned int &height, string &fileName);	///< Crée une nouvelle matrice ayant pour nom du fichier: fileName.
+	/// \fn bool newMatrix(unsigned int const& width, unsigned int const& height, string const& fileName)
+	/// \brief Création d'une nouvelle matrice.
+	///
+	/// Va créer un nouveau fichier et initialiser les valeurs 
+	/// width et heigth. 
+	/// Attention : On ne crée rien si le fichier existe déjà.
+	/// \param width la largeur de la matrice.
+	/// \param height la hauteur de la matrice.
+	/// \param fileName le nom du fichier que l'on veut créer.
 	
-	//! Remplissage random de la matrice.
-	/*!
-		Va remplir la matrice de manière aléatoire avec des valeurs
-		comprises entre min et max.
-		Attention : Remplace les valeurs déjà existantes.
-		\param min la valeur minimale.
-		\param max la valeur maximale.
-	*/
-	void random(unsigned const& int min, unsigned int const&max);						///< On remplie la matrice aléatoirement. 
+	bool newMatrix(unsigned int const& width, unsigned int const& height, string const& fileName);	///< Crée une nouvelle matrice ayant pour nom du fichier: fileName.
 	
-	//! Renommer le fichier
-	/*!
-		Renomme le nom du fichier sur le disque dur. A ne pas
-		confondre avec setName qui correspond au nom de la 
-		matrice dans le programme.
-		\param fileName nouveau nom du fichier.
-	*/
-	void renameFile(string &fileName);			///< Renomme le fichier.
+	/// \fn void random(unsigned int const& min, unsigned int const& max)
+	/// \brief Remplissage random de la matrice.
+	///
+	/// Va remplir la matrice de manière aléatoire avec des valeurs
+	/// comprises entre min et max.
+	/// Attention : Remplace les valeurs déjà existantes.
+	/// \param min la valeur minimale.
+	/// \param max la valeur maximale.
 	
-	//! Vérifier si la matrice est configurée
-	/*
-		Vérifie si la matrice est bien configurée, soit si
-		elle est liée à un fichier. Si tel est le cas, 
-		retourne vrai, sinon retourne faux. 
-	*/
-	bool isSet();	
+	void random(unsigned const& int min, unsigned int const& max);						///< On remplie la matrice aléatoirement. 
+	
+	/// \fn void renameFile(string const& fileName)
+	/// \brief Renommer le fichier
+	///
+	/// Renomme le nom du fichier sur le disque dur. A ne pas
+	/// confondre avec setName qui correspond au nom de la 
+	/// matrice dans le programme.
+	/// \param fileName nouveau nom du fichier.
+	
+	void renameFile(string const&fileName);			///< Renomme le fichier.
+	
+	/// \fn bool isSet() const
+	/// \brief Vérifier si la matrice est configurée
+	///
+	/// Vérifie si la matrice est bien configurée, soit si
+	/// elle est liée à un fichier. Si tel est le cas, 
+	/// retourne vrai, sinon retourne faux. 
+
+	bool isSet() const;	
 
 /************************** ACCESSORS AND MUTATORS *************************/
 	
-	//! Retourne la largeur.
-	/*
-		Retourne la largeur de la matrice.
-	*/	
+	/// \fn unsigned int getWidth() const
+	/// \brief Retourne la largeur.
+	///
+	/// Retourne la largeur de la matrice.
+	///
 	unsigned int getWidth()const;				///< Récupère la largeur.
 	
-	//! Retourne la hauteur
-	/*
-		Retourne la hauteur de la matrice.
-	*/
+	/// \fn unsigned int getHeight() const
+	/// \brief Retourne la hauteur
+	///
+	/// Retourne la hauteur de la matrice.
+
 	unsigned int getHeight()const;				///< Modifie la largeur.
 	
-	//! Retourne le nom du fichier.
-	/*
-		Retourne le nom du fichier. 
-		A ne pas confondre avec le nom de la matrice.
-	*/
+	/// \fn string getFileName() const
+	/// \brief Retourne le nom du fichier.
+	///
+	/// Retourne le nom du fichier. 
+	/// A ne pas confondre avec le nom de la matrice.
+
 	string getFileName()const;				///< On récupère le nom du fichier.
 	
-	//! Retourne la nom de la matrice.
-	/*
-		Retourne le nom de la matrice. 
-		A ne pas confondre avec le nom du fichier.
-	*/
+	/// \fn string getName() const
+	/// \brief Retourne la nom de la matrice.
+	///
+	/// Retourne le nom de la matrice. 
+	/// A ne pas confondre avec le nom du fichier.
+
 	string getName() const;					///< Récupère le nom.	
 
-	//! Modifie le nom du fichier.
-	/*
-		Modifie le nom du fichier. Il vérifie aussi si
-		l'extension est bonne.
-		\param fileName le nouveau nom du fichier.
-	*/
-	void setFileName(string &fileName);			///< On modifie le nom du fichier et on récupère ses valeurs.
+	/// \fn void setFileName(string const& fileName)
+	/// \brief Modifie le nom du fichier.
+	///
+	/// Modifie le nom du fichier. Il vérifie aussi si
+	/// l'extension est bonne.
+	/// \param fileName le nouveau nom du fichier.
 	
-	//! Modifie le nom de la matrice
-	/*
-		Modifie le nom de la matrice. 
-		\param name le nouveau nom de la matrice.
-	*/
-	void setName(string &name);				///< On modifie le nom de la matrice.
+	void setFileName(string const& fileName);			///< On modifie le nom du fichier et on récupère ses valeurs.
+	
+	/// \fn void setName(string const& name)
+	/// \brief Modifie le nom de la matrice
+	///
+	/// Modifie le nom de la matrice. 
+	/// \param name le nouveau nom de la matrice.
+	
+	void setName(string const& name);				///< On modifie le nom de la matrice.
 private:
 
-	//! Modifie la largeur
-	/* 
-		Modifie la largeur de la matrice. Celle
-		-ci n'est utilisable que par la classe 
-		car elle ne permet que sa configuration.
-		L'utilisateur n'y a pas accès.
-		\param width la nouvelle largeur de la matrice.
-	*/
-	void setWidth(unsigned int width);
+	/// \fn void setWidth(unsigned int const& width)
+	/// Modifie la largeur
+	///
+	/// Modifie la largeur de la matrice. Celle
+	/// -ci n'est utilisable que par la classe 
+	/// car elle ne permet que sa configuration.
+	/// L'utilisateur n'y a pas accès.
+	/// \param width la nouvelle largeur de la matrice.
 	
-	//! Modifie la hauteur
-	/*
-		Modifie la hauteur de la matrice. Celle
-		-ci n'est utilisable que par la classe
-		car elle ne permet que sa configuration.
-		L'utilisateur n'y a pas accès.
-		\param height la nouvelle hauteur de la matrice.
-	*/
-	void setHeight(unsigned int height);
+	void setWidth(unsigned int const& width);
+	
+	/// \fn void setHeight(unsigned int const& height)
+	/// \brief Modifie la hauteur
+	///
+	/// Modifie la hauteur de la matrice. Celle
+	/// -ci n'est utilisable que par la classe
+	/// car elle ne permet que sa configuration.
+	/// L'utilisateur n'y a pas accès.
+	/// \param height la nouvelle hauteur de la matrice.
+	
+	void setHeight(unsigned int const& height);
 };
