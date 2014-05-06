34c3f5153fcd40abc8cf7968c78f4a78c8b4b403
Serre Jérémy <offvolt@gmail.com>
2014-05-06 17:45:54 +0200

    Update converter.cpp
    Ajout de la fonction verif qui permet la verification du fichier par jerem ;)

--- src/matrix++/converter.cpp
+++ src/matrix++/converter.cpp
@@ -99,3 +99,96 @@ void Converter::convert(string &fileName1,string &fileName2)
 }
 
 
+bool Converter::verif(string &fileName)
+{
+    ifstream fichier(fileName);
+    string nom="tmp.txt";
+    
+    Converter::convertbin(fileName,nom); // Creation d'un fichier temporaire en binaire
+    ifstream fichiertmp(nom);
+    
+    
+    int a;
+    int val(0),m(2),m2(3),m3(4),x(0);
+    
+    vector<int> tab(0);
+    
+    while(fichiertmp.read ((char *)&a, sizeof(int)) )
+    {
+        tab.push_back(a); // Recupere toutes les données de la matrice
+    }
+
+    int sizeTab=tab.size();
+    vector<int> tabLigne(0);
+    vector<int> tabColonne(0);
+    vector<int> tabValeur(0);
+    
+    for(int i=2 ; i<sizeTab; i++)
+    {
+        if(m==i)
+        {
+            tabLigne.push_back(tab[i]); // Stocke tous les indices des lignes
+            m=m+3;
+        }
+        if(m2==i)
+        {
+            tabColonne.push_back(tab[i]); // Stocke tous les indices des Colonnes
+            m2=m2+3;
+        }
+        if(m3==i)
+        {
+            tabValeur.push_back(tab[i]); // Stocke tous les valeur
+            m3=m3+3;
+        }
+    }
+    
+    for(int i=0 ; i<tabColonne.size(); i++) // Verifie si les colonnes en ordre croissant
+    {
+        if(tabColonne[i]>tabColonne[i+1] && i<tabColonne.size()-1) // pour eviter le depassement
+        {
+            cout<<"Probleme apres le colonne ["<<tabColonne[i]<<"]"<<endl;
+            return false;
+        }
+    }
+    
+    for(int i=0 ; i<tabLigne.size(); i++) // Verifie si les ligne en ordre croissant
+    {
+        if(tabLigne[i]>tabLigne[i+1] && i<tabLigne.size()-1)
+        {
+            cout<<"Probleme apres la ligne ["<<tabLigne[i]<<"]"<<endl;
+            return false;
+        }
+    }
+    
+    for(int i=0 ; i<tabValeur.size(); i++) // Verifie si une valeur == 0
+    {
+        if(tabValeur[i]==0)
+        {
+            cout<<"ERREUR : Dans le fichier contenant la matrice on ne doit avoir mat[i][j]=0"<<endl;
+            return false;
+        }
+    }
+    
+    // Verification si chaque colone a une valeur
+    
+    
+    for(int i=0 ; i<tabColonne.size(); i++) // Verifie si les colonnes en ordre croissant
+    {
+        for(int j=0 ; j<tabColonne.size(); j++) // Verifie si les colonnes en ordre croissant
+        {
+            if(i==tabColonne[j]) x=1;
+        }
+        if(x!=1)
+        {
+            cout<<"ERREUR : La colonne "<<i<<" n'est pas remplie"<<endl;
+            return false;
+        }
+        x=0;
+    }
+    
+    remove("tmp.txt"); // Supprime le fichier temporaire
+    fichier.close();
+    fichiertmp.close();
+    
+    return true;
+}
