ac03331e440b1041da1ef39ab40a0537de58e4e5
Serre Jérémy <offvolt@gmail.com>
2014-05-06 21:22:03 +0200

    Update converter.h
    Doxygen pour la fonction verif

--- headers/matrix++/converter.h
+++ headers/matrix++/converter.h
@@ -2,6 +2,7 @@
  * \file converter.h
  * \brief Convertisseur de matrice (en format .txt par exemple) en fichier binaire avec tous les chiffres a la suite 
  * et de binaire en fichier (txt  par exemple) sous forme de matrice
+ * Deplus on une fonction de verification de la matrice.
  * \return Rien - Arrêt normal du programme.
  * \author SERRE Jeremy
  * \version 1.0
@@ -39,5 +40,16 @@ class Converter
     */   
     
     static bool verif(string &fileName); // Fonction de verification
+   /*!
+    * \brief Est une fonction de Verification que la matrice respecte les conditions qui sont les suivantes:
+    *    - Il faut que les colonnes dans la matrice soit dans l'ordre croissant
+    *    - Il faut que les lignes de la matrice dans le fichier soit dans l'ordre croissante
+    *    - Il faut que le fichier ne garde que les valeurs differente de zero matrice[i][j]!=0
+    *    - Il faut que chaque colonne est au moins une valeur differente de zero c'est une condition
+    * Si il y a un probleme l'utilisateur sera avertie d'ou vien le probleme dans le fichier.
+    * \param FileName :Chaîne de caractere (string) qui est le fichier de lecture.
+    *        FileName ne peuvent pas être nul sinon il ce passera rien
+    *\return true si la fichier et correcte sinon il retourne false
+    
 };
 
