752e543155a675febaa1ff3eb45937174cd85ee5
Serre Jérémy <offvolt@gmail.com>
2014-05-06 14:58:54 +0200

    Update converter.h
    Fichier final

--- headers/matrix++/converter.h
+++ headers/matrix++/converter.h
@@ -1,16 +1,20 @@
-#ifndef H_CONVERTISER
-#define H_CONVERTISER
+#include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
 class Converter
 {
-private:
-	// Ici seront écrites les fonctions utilitaires
-	// Dont convert a besoin et dont l'utilisateur n'a pas le droit
-	// D'accès.
-public:
-	static void convert(string &fileName1, string &fileName2);
+    private:
+    
+    public:
+    Converter();
+    static void convertbin(string &fileName1,string &fileName2);
+    static void convert(string &fileName1,string &fileName2);
+    // fileName on comme nom de fchier fileName.txt a l'interieurs
+    
 };
 
-#endif
+// Converter::convert(string...)
+// on fait sa si il n'y a pas de construction
