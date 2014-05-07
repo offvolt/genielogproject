8847c727fc24e951dd7a9d0b08abc8f162ceca64
Serre Jérémy <offvolt@gmail.com>
2014-05-07 18:03:26 +0200

    Update converter.h
    Ajout te la fonction verif dans convert a titre consultatif donc ne bloquera pas les calculs.
    Deplus j'ai crée  et modifié une fonction convertbin(string &fileName1,string &fileName2) et convertbin2(string &fileName1,string &fileName2)
    J'ai du faire ca car si j'inclue verif dans convertebin2 alors verif appel la fonction convertbin2 et convertbin2 appel verif donc c'est une boucle infini d'ou l'utilité de une fonction qui les sépare distinctement.

--- headers/matrix++/converter.h
+++ headers/matrix++/converter.h
@@ -20,10 +20,18 @@ class Converter
     private:
     
     public:
-    Converter();
-    static void convertbin(string &fileName1,string &fileName2); // Fichier "lisible" vers binaire
-    /*!
-    * \brief Convertisseur de matrice (en format .txt par exemple) en fichier binaire avec tous les chiffres à la suite 
+    static void convertbin(string &fileName1,string &fileName2);
+   /*!
+    * \brief Appel la fonction convertbin2 et verif pour permettre la verification si le fichier est conforme.
+    * La fonction verif ne bloque pas le convertisseur mais indique que le fichier contenant la matrice n'est pas conforme.
+    * \param FileName1 :Chaîne de caractere (string) qui est le fichier de lecture,
+    *        FileName2 :Chaîne de caractere (string) qui est le fichier de d'ecriture soit de destination.
+    * Si le fichier de destination n'existe pas il le crée automatiquement au nom donnée.
+    * FileName1 et FileName2 ne peuvent pas être nul.
+    */
+    static void convertbin2(string &fileName1,string &fileName2); // Fichier "lisible" vers binaire
+   /*!
+    * \brief Convertisseurbin2 de matrice (en format .txt par exemple) en fichier binaire avec tous les chiffres à la suite 
     * \param FileName1 :Chaîne de caractere (string) qui est le fichier de lecture,
     *        FileName2 :Chaîne de caractere (string) qui est le fichier de d'ecriture soit de destination.
     * Si le fichier de destination n'existe pas il le crée automatiquement au nom donnée.
@@ -31,7 +39,7 @@ class Converter
     */
 
     static void convert(string &fileName1,string &fileName2); // Fichier binaire vers un fichier "lisible"
-    /*!
+   /*!
     * \brief Convertisseur de fichier ecrit en binaire vers un fichier sous forme de matrice (en format .txt par exemple) 
     * \param FileName1 :Chaîne de caractere (string) qui est le fichier de lecture,
     *        FileName2 :Chaîne de caractere (string) qui est le fichier de d'ecriture soit de destination.
@@ -39,7 +47,7 @@ class Converter
     * FileName1 et FileName2 ne peuvent pas être nul.
     */   
     
-    static bool verif(string &fileName); // Fonction de verification
+    static void verif(string &fileName); // Fonction de verification
    /*!
     * \brief Est une fonction de Verification que la matrice respecte les conditions qui sont les suivantes:
     *    - Il faut que les colonnes dans la matrice soit dans l'ordre croissant
@@ -47,9 +55,12 @@ class Converter
     *    - Il faut que le fichier ne garde que les valeurs differente de zero matrice[i][j]!=0
     *    - Il faut que chaque colonne est au moins une valeur differente de zero c'est une condition
     * Si il y a un probleme l'utilisateur sera avertie d'ou vien le probleme dans le fichier.
+    * Elle permet uniquement d'indiquer les erreurs à l'utilisateur.
+    * Parcontre on peut la faire retourner si il y a une erreur ou non en enlevant les commentaire au return
+    * en transformant la fonction de void en bool.
     * \param FileName :Chaîne de caractere (string) qui est le fichier de lecture.
     *        FileName ne peuvent pas être nul sinon il ce passera rien
-    *\return true si la fichier et correcte sinon il retourne false
+    *\return Il retourne rien car elle sers uniquement à la consultation
     */
 };
 
