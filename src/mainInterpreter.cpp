f480b592e591abb44b86df7ccf2d7b97a9c84669
Ariel <ariel.roman@hotmail.fr>
2014-05-16 10:50:32 +0200

    Update mainInterpreter.cpp

--- src/mainInterpreter.cpp
+++ src/mainInterpreter.cpp
@@ -6,12 +6,18 @@ using namespace std;
 int main()
 {
     bool val;
-    string var="";
-    cout << "Taper une commande" << endl;
-    cin >> var;
     Interpreter I;
-    I.setCommand(var);
-    val=I.activeProcess();
-    if (val == false) { cout << "Erreur de saisie" << endl; }
-    cout << "Derniere commande en memoire : " << I.getCommand() << endl;
+    string var="";
+    while (var != "stop")
+    {
+        cout << "Taper une commande (stop pour arreter) :" << endl;
+        cin >> var;
+        if (var !="stop")
+        {
+            I.setCommand(var);
+            val=I.activeProcess();
+            if (val == false) { cout << "Erreur de saisie" << endl; }
+            cout << "Derniere commande en memoire : " << I.getCommand() << endl;
+        }
+    }
 }
