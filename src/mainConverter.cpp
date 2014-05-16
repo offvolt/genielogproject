09a3d0c1c65d490275910bd32c18cd7b1743a4a3
Serre Jérémy <offvolt@gmail.com>
2014-05-16 10:38:03 +0200

    Create mainConverter.cpp
    Main pour test la class converter

---
+++ src/mainConverter.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "Converter.h"
+#include <fstream> 
+
+using namespace std;
+
+int main()
+{
+    string fichL,fichE,fichL1,fichE2; // fichier lecture, fichier ecriture
+    fichL="mat.txt";
+    fichE="mat2.txt";
+    fichL1="mat2.txt";
+    fichE2="mat3.txt";
+    Converter::convertbin(fichL,fichE);
+    Converter::convert(fichL1,fichE2);
+    return 0;
+}
