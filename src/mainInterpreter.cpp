8f5d93219478afeb40322ee1150bdb55862dce14
Quillot Mathias <adm-2w22a-06.univ-avignon.fr>
2014-05-16 12:27:33 +0200

    merge du mainInterpreter

--- src/mainInterpreter.cpp
+++ src/mainInterpreter.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
 	MatrixManager m;
 	bool val;
     	string var="";
@@ -17,21 +16,4 @@ int main()
     	val=I.activeProcess(m);
     	if (val == false) { cout << "Erreur de saisie" << endl; }
     		cout << "Derniere commande en memoire : " << I.getCommand() << endl;
-=======
-    bool val;
-    Interpreter I;
-    string var="";
-    while (var != "stop")
-    {
-        cout << "Taper une commande (stop pour arreter) :" << endl;
-        cin >> var;
-        if (var !="stop")
-        {
-            I.setCommand(var);
-            val=I.activeProcess();
-            if (val == false) { cout << "Erreur de saisie" << endl; }
-            cout << "Derniere commande en memoire : " << I.getCommand() << endl;
-        }
-    }
->>>>>>> f480b592e591abb44b86df7ccf2d7b97a9c84669
 }
